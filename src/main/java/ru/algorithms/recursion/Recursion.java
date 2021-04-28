3ac9ddade68e0e415b237ab1bd3c832b514ed228
Igor Gilev <gilev_ia@proitr.ru>
2021-04-28 23:19:53 +0500

    Сумма элементов списка рекурсивно.

---
+++ src/main/java/ru/algorithms/recursion/Recursion.java
@@ -0,0 +1,20 @@
+package ru.algorithms.recursion;
+
+import java.util.Stack;
+
+public class Recursion {
+
+    /**
+     * Рекурсивный метод, вычисляющий сумму элементов списка
+     * @param list - список
+     */
+    public static int reqSum(Stack<Integer> list) {
+        if (list.size() == 0) {
+            return 0;
+        }
+        if (list.size() == 1) {
+            return list.pop();
+        }
+        return list.pop() + reqSum(list);
+    }
+}
---
+++ src/test/java/ru/algorithms/recursion/RecursionTest.java
@@ -0,0 +1,44 @@
+package ru.algorithms.recursion;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Stack;
+
+public class RecursionTest {
+    private final Stack<Integer> list = new Stack<>();
+
+    @Before
+    public void init() {
+        list.clear();
+    }
+
+    @Test
+    public void sum15Test() {
+
+        list.push(5);
+        list.push(4);
+        list.push(3);
+        list.push(2);
+        list.push(1);
+
+        int result = Recursion.reqSum(list);
+        Assert.assertEquals(15, result);
+    }
+
+    @Test
+    public void sumOneElemTest() {
+        list.push(23);
+        Assert.assertEquals(1, list.size());
+        Assert.assertEquals(23, Recursion.reqSum(list));
+    }
+
+    @Test
+    public void sumZeroElemTest() {
+        Assert.assertEquals(0, list.size());
+        Assert.assertEquals(0, Recursion.reqSum(list));
+    }
+
+
+}
